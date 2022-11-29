ce95ca4da2f0cb1b6045ef721923fa55e46cffb1
hattori_akio@hotmail.com <hattori_akio@hotmail.com>
2022-11-29 15:36:58 -0300

    Subindo exercicio 2 de Javascript atualizado/finalizado

--- inicio/exercicios/02/index.js
+++ inicio/exercicios/02/index.js
@@ -5,17 +5,25 @@ const p = document.getElementById('confirma')
 function entrar(e){
     e.preventDefault()
     p.innerText = ""
+    const log = document.getElementById("login")
     const usu = document.getElementsByName("usuario")[0].value
     const sen = document.getElementsByName("password")[0].value
     if(usu == "" || sen == ""){
         p.innerText = 'Ambos os campos devem ser preenchidos'
         alert("Os campos são obrigatórios")
+    }else{
         if(usu == "Akio" && sen == "1234"){
             p.innerText = 'Logado'
+            if(log.style.display === "none"){
+                log.style.display = "block"
+            }else{
+                log.style.display = "none"
+            }
         }else{
-            p.innerText = 'Usuário ou senha incorretos!'
+            p.innerText = 'Preencha corretamente os campos'
         }
     }
     document.getElementsByName("usuario")[0].value = ""
+    document.getElementsByName("usuario")[0].focus()
     document.getElementsByName("password")[0].value = ""
 }
